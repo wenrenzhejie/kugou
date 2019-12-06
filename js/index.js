f09742872ca62ebfa70671905af2c35517e58e3f
xin <xin@qq.com>
2019-12-06 11:59:31 +0800

    Listen主页完成

--- js/index.js
+++ js/index.js
@@ -1,5 +1,43 @@
 $(document).on("pagecreate","#pageIndex",function(){
-    $(".myNav1 ul li").on("tap",function(){
-        console.log("aaaaaaaaa");
+    $(".index_nav_me").on("tap",function(){
+        console.log("index");
+        $.mobile.changePage("index.html","slideup");
+    });
+    $(".index_nav_listen").on("tap",function(){
+        console.log("listen");
+        $.mobile.changePage("listen.html","slideup");
+    });
+    $(".index_nav_look").on("tap",function(){
+        $.mobile.changePage("look.html","slideup");
+    });
+});
+$(document).on("pagecreate","#pageListen",function(){
+    lunbotu();
+    function lunbotu(){
+        console.log("Jinlaile");
+        let imageArray = ["../images/1.jpg","../images/2.jpg","../images/3.jpg","../images/4.jpg","../images/5.jpg"];
+        let temp;
+        if (temp == null){
+            temp = setInterval(fn,3000);
+        }
+        function fn() {
+            let index = parseInt($("#imgIndex").val());
+            index = ++index%5;
+            let bg = "url(" + imageArray[index] + ")";
+            $("#lunbotu").css("backgroundImage",bg);
+            $("#imgIndex").val(index);
+        }
+
+    }
+    $(".index_nav_me").on("tap",function(){
+        console.log("index");
+        $.mobile.changePage("index.html","slideup");
+    });
+    $(".index_nav_listen").on("tap",function(){
+        console.log("listen");
+        $.mobile.changePage("listen.html","slideup");
+    });
+    $(".index_nav_look").on("tap",function(){
+        $.mobile.changePage("look.html","slideup");
     });
 });
